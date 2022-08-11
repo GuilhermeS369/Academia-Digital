8fc6e5e0d4abf78b413fb3a31bc69a16b51c64ea
Guilherme Santos <guilherme460@yahoo.com.br>
2022-08-10 22:34:32 -0300

    Aluno Insert, Update, Delete

--- src/main/java/com/bootcamp/resources/AlunoResource.java
+++ src/main/java/com/bootcamp/resources/AlunoResource.java
@@ -1,35 +1,87 @@
 package com.bootcamp.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bootcamp.entities.Aluno;
 import com.bootcamp.services.AlunoService;
+import com.bootcamp.services.exceptions.DatabaseException;
+import com.bootcamp.services.exceptions.ResourceNotFoundException;
+
 @RestController
 @RequestMapping(value = "/aluno")
 public class AlunoResource {
 	@Autowired
 	private AlunoService alunoService;
-	
+
+	@Autowired
+
+	// ----------------------------------------------------------------------------
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Aluno> findById(@PathVariable Long id){
+	public ResponseEntity<Aluno> findById(@PathVariable Long id) {
 		Aluno aluno = alunoService.findById(id);
-		
+
 		return ResponseEntity.ok().body(aluno);
 	}
-	
+
+	// ----------------------------------------------------------------------------
 	@GetMapping
-	public ResponseEntity<List<Aluno>> findAll(){
-		
+	public ResponseEntity<List<Aluno>> findAll() {
+
 		List<Aluno> listaAluno = alunoService.findAll();
-		
+
 		return ResponseEntity.ok().body(listaAluno);
 	}
-	
+
+	// ----------------------------------------------------------------------------
+	@PostMapping
+	public ResponseEntity<Aluno> insert(@RequestBody Aluno obj) {
+		obj = alunoService.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	// ----------------------------------------------------------------------------
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Aluno> update(@PathVariable Long id, @RequestBody Aluno obj) {
+
+		obj = alunoService.update(id, obj);
+
+		return ResponseEntity.ok().body(obj);
+	}
+
+//----------------------------------------------------------------------------
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		try {
+			alunoService.delete(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+			
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+			
+		}
+		
+
+		return ResponseEntity.noContent().build();
+
+	}
+
 }
--- src/main/java/com/bootcamp/services/AlunoService.java
+++ src/main/java/com/bootcamp/services/AlunoService.java
@@ -3,11 +3,16 @@ package com.bootcamp.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.bootcamp.entities.Aluno;
 import com.bootcamp.repositories.AlunoRepository;
+import com.bootcamp.services.exceptions.DatabaseException;
 import com.bootcamp.services.exceptions.ResourceNotFoundException;
 @Service
 public class AlunoService {
@@ -29,5 +34,48 @@ public class AlunoService {
 	public Aluno insert(Aluno aluno){
 		return alunoRepository.save(aluno);
 	}
+	
+	public void delete(Long id) {
+		try {
+			alunoRepository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+			
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+			
+		}
+	}
+	
+	public Aluno update (Long id, Aluno obj) {
+		try {
+			
+			Aluno entity = alunoRepository.getReferenceById(id);
+			
+			updateData(entity, obj);
+			
+			return alunoRepository.save(entity);
+			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
+	
+	}
+
+	private void updateData(Aluno entity, Aluno obj) {
+		entity.setName(obj.getName());
+		entity.setBairro(obj.getBairro());
+		entity.setCpf(obj.getCpf());
+		entity.setDataDeNascimento(obj.getDataDeNascimento());
+				
+	}
+	
+	
+	
+	
+	
+	
 	
 }
